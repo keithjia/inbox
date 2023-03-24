bdeef5173acf765094079b8c6d94663c758b661e
user23 <user23@aws.com>
2023-03-23 21:10:22 -0700

    update

--- deploy.js
+++ deploy.js
@@ -2,6 +2,8 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 //const {abi, evm} = require("./compile");
 const contracts = require("./compile");
+const util = require("util");
+
 const gorliNetwork = "https://goerli.infura.io/v3/ba39c821796547c3a391077c22151360";
 
 const truffleWalletProvider = new HDWalletProvider(
@@ -20,7 +22,7 @@ const web3 = new Web3(truffleWalletProvider);
 
     await deployContract(["first real contract."], contracts["Inbox.sol"].Inbox, testFromAccount);
     await deployContract(null, contracts["Lottery.sol"].Lottery, testFromAccount);  
-    
+
     // deployedInboxContract = await new web3.eth.Contract(abi)
     // .deploy({
     //     data: evm.bytecode.object, 
@@ -30,21 +32,21 @@ const web3 = new Web3(truffleWalletProvider);
     //     gas:1000000});
 
     //     console.log(`contract deployed to: ${deployedInboxContract.options.address}`);
-    
-    
+
     truffleWalletProvider.engine.stop();
 })();
 
-async function deployContract(arguments, bytecodeObj, from) {
+async function deployContract(arguments, contract, from) {
     // creates the contract, send() issues the trasaction and 
     // executes the deployment
-    deployedInboxContract = await new web3.eth.Contract(abi)
+    deployedInboxContract = await new web3.eth.Contract(contract.abi)
     .deploy({
-        data: bytecodeObj, 
+        data: contract.evm.bytecode.object, 
         arguments: arguments})
     .send({
         from: from, 
         gas:1000000});
 
+    console.log(util.inspect(contract.abi, {showHidden: false, depth: null, colors: true}))
     console.log(`contract deployed to: ${deployedInboxContract.options.address}`);
 }
