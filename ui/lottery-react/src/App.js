87273ce0b863925a2d1cc8991e56ef2976b084fb
keith jpmc mac2 <jiakeith@gmail.com>
2023-03-24 19:34:39 -0700

    complete react app for lottery

--- ui/lottery-react/src/App.js
+++ ui/lottery-react/src/App.js
@@ -6,14 +6,19 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [isEntering, setIsEnter] = useState(false);
+
   const [manager, setManager] = useState();
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState(0);
   const [value, setValue] = useState("");
+  const [message, setMsg] = useState("");
+
 
   useEffect(() => {
     (async () => {
       setLoading(true);
+      setIsEnter(false);
 
       setManager(await lottery.methods.manager().call());
       setPlayers(await lottery.methods.getPlayers().call());
@@ -26,11 +31,33 @@ function App() {
   const onSubmit = async (event) => {
     event.preventDefault();
 
+    setMsg( "waiting on trx success");
+    setIsEnter(true);
+
     const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
     await lottery.methods.enter().send({
       from: accounts[0],
+      gas: 1000000,
       value: value
     });
+
+    setMsg("you've been entered");
+
+    setPlayers(await lottery.methods.getPlayers().call());
+    setBalance(await lottery.methods.getTotalPrizePool().call());
+    setIsEnter(false);
+
+  };
+
+  const onPickWinner = async (event)=> {
+    //const accounts = await web3.eth.getAccounts();
+    setMsg( "drawing form a pool ...");
+    await lottery.methods.pickWinner().send({from: manager});
+
+    setMsg("winner drawn!!");
+    setPlayers(await lottery.methods.getPlayers().call());
+    setBalance(await lottery.methods.getTotalPrizePool().call());
   };
 
   return (
@@ -51,11 +78,19 @@ function App() {
           </div>
         </p>
         <hr></hr>
-        <form onSubmit={onSubmit}>
+        {isEntering ? <div>wait...</div> :
+          <form onSubmit={onSubmit}>
           <h4>Enter Lottery</h4>
           <input onChange={event => setValue(event.target.value)} value={value}/>
           <button>enter</button>
-        </form>
+        </form> 
+        }
+          
+        
+        <h2>{message}</h2>
+        <hr></hr>
+        <h4>pick winner</h4>
+        <button onClick={onPickWinner}>Go!</button>
         <p>
           Edit <code>src/App.js</code> and save to reload. this is pretty cool
         </p>
